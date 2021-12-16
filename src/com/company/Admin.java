d3d68859f0661496da077f1f93ec5bfb9d49b3f8
Glebii <60538108+Glebii@users.noreply.github.com>
2021-12-16 18:23:31 +0600

    Gleb Admin get menu

--- src/com/company/Admin.java
+++ src/com/company/Admin.java
@@ -15,8 +15,57 @@ public class Admin extends Cashier{
 
     @Override
     public void getMenu() throws InterruptedException, SQLException, IOException, ClassNotFoundException {
-        super.getMenu();
+        System.out.println("Чего желаете?\n1) Увидеть все материалы\n2) Посмотреть материал по Id\n3) Посмотреть материал по бренду\n4) Увидеть всех поставщиков\n5) Посмотреть данные поставщика по Id\n6) Сделать продажу\n7) Увидеть все продажи\n8) Завершить работу\nВведите номер операции:");
+        Scanner sc = new Scanner(System.in);
+        int operationNumber = sc.nextInt();
+        switch (operationNumber) {
+            case 1:
+                getAllMaterials();
+                Thread.sleep(2000);
+                getMenu();
+                break;
+            case 2:
+                searchMatById();
+                Thread.sleep(2000);
+                getMenu();
+                break;
+            case 3:
+                searchMaterialByBrand();
+                Thread.sleep(2000);
+                getMenu();
+                break;
+            case 4:
+                getAllSuppliers();
+                Thread.sleep(2000);
+                getMenu();
+                break;
+            case 5:
+                getSupByID();
+                Thread.sleep(2000);
+                getMenu();
+                break;
+            case 6:
+                makeASale();
+                Thread.sleep(2000);
+                getMenu();
+                break;
+            case 7:
+                getAllTransactions();
+                Thread.sleep(2000);
+                getMenu();
+            case 8:
+                System.out.printf("Отличная работа ,%s", getName());
+                break;
+            case 9:
+                System.out.println("привет");
+                break;
+            default:
+                System.out.println("Номер операции введен не верно, попробуйте снова!");
+                getMenu();
+                break;
 
+
+        }
     }
 
     //Add new user
